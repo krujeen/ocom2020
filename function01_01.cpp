0646b6061ec021a062df857789e8f88852859c31
KruJeen <44271005+krujeen@users.noreply.github.com>
2021-04-18 14:05:24 +0700

    Create function01_01.cpp

---
+++ function01_01.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+using namespace std;
+
+void line(){
+    printf("-----------------");
+}
+
+int main ()
+{
+    printf ("Hello");
+    line ();
+    printf ("Good bye");
+    line ();
+    return 0;
+
+}
